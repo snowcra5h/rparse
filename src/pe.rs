de870a62def79b0e83d1282c977594d22f3c85d9
snowcrash <snowcra5h@icloud.com>
2023-04-08 00:58:13 -0600

    test commit

---
+++ src/pe.rs
@@ -0,0 +1,108 @@
+use byteorder::{LittleEndian, ReadBytesExt};
+use std::error::Error;
+use winapi::um::winnt::IMAGE_DOS_HEADER;
+
+pub struct Pe {
+    pub dos_header: IMAGE_DOS_HEADER,
+}
+
+impl Pe {
+    pub fn new() -> Pe {
+        Pe {
+            dos_header: IMAGE_DOS_HEADER {
+                e_magic: 0,
+                e_cblp: 0,
+                e_cp: 0,
+                e_crlc: 0,
+                e_cparhdr: 0,
+                e_minalloc: 0,
+                e_maxalloc: 0,
+                e_ss: 0,
+                e_sp: 0,
+                e_csum: 0,
+                e_ip: 0,
+                e_cs: 0,
+                e_lfarlc: 0,
+                e_ovno: 0,
+                e_res: [0; 4],
+                e_oemid: 0,
+                e_oeminfo: 0,
+                e_res2: [0; 10],
+                e_lfanew: 0,
+            },
+        }
+    }
+
+    pub fn parse_dos_header(
+        &mut self,
+        reader: &mut std::io::BufReader<std::fs::File>,
+    ) -> Result<(), Box<dyn Error>> {
+        self.dos_header.e_magic = reader.read_u16::<LittleEndian>()?;
+
+        if self.dos_header.e_magic != 0x5A4D {
+            return Err("Invalid DOS header".into());
+        }
+
+        self.dos_header.e_cblp = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_cp = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_crlc = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_cparhdr = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_minalloc = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_maxalloc = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_ss = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_sp = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_csum = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_ip = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_cs = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_lfarlc = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_ovno = reader.read_u16::<LittleEndian>()?;
+        for i in 0..4 {
+            self.dos_header.e_res[i] = reader.read_u16::<LittleEndian>()?;
+        }
+        self.dos_header.e_oemid = reader.read_u16::<LittleEndian>()?;
+        self.dos_header.e_oeminfo = reader.read_u16::<LittleEndian>()?;
+        for i in 0..10 {
+            self.dos_header.e_res2[i] = reader.read_u16::<LittleEndian>()?;
+        }
+        self.dos_header.e_lfanew = reader.read_i32::<LittleEndian>()?;
+
+        Ok(())
+    }
+
+    pub fn print_dos_header(&self) {
+        println!("DOS Header:");
+        println!("  e_magic:      {:#06X}", self.dos_header.e_magic);
+        println!("  e_cblp:       {:#06X}", self.dos_header.e_cblp);
+        println!("  e_cp:         {:#06X}", self.dos_header.e_cp);
+        println!("  e_crlc:       {:#06X}", self.dos_header.e_crlc);
+        println!("  e_cparhdr:    {:#06X}", self.dos_header.e_cparhdr);
+        println!("  e_minalloc:   {:#06X}", self.dos_header.e_minalloc);
+        println!("  e_maxalloc:   {:#06X}", self.dos_header.e_maxalloc);
+        println!("  e_ss:         {:#06X}", self.dos_header.e_ss);
+        println!("  e_sp:         {:#06X}", self.dos_header.e_sp);
+        println!("  e_csum:       {:#06X}", self.dos_header.e_csum);
+        println!("  e_ip:         {:#06X}", self.dos_header.e_ip);
+        println!("  e_cs:         {:#06X}", self.dos_header.e_cs);
+        println!("  e_lfarlc:     {:#06X}", self.dos_header.e_lfarlc);
+        println!("  e_ovno:       {:#06X}", self.dos_header.e_ovno);
+        print!("  e_res:");
+        for i in 0..4 {
+            if i == 0 {
+                print!("\t");
+            } else {
+                print!("{:#06X} ", self.dos_header.e_res[i]);
+            }
+        }
+        println!("\n  e_oemid:      {:#06X}", self.dos_header.e_oemid);
+        println!("  e_oeminfo:    {:#06X}", self.dos_header.e_oeminfo);
+        print!("  e_res2:");
+        for i in 0..10 {
+            if i == 0 {
+                print!("\t");
+            } else {
+                print!("{:#06X} ", self.dos_header.e_res2[i]);
+            }
+        }
+        println!("\n  e_lfanew:     {:#010X}\n", self.dos_header.e_lfanew);
+    }
+}
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,9 +1,47 @@
 use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
 
-fn init_term() -> Result<(), io::Error> {
-    let backend = CrosstermBackend::new(io::stdout());
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Widget},
+    Terminal,
+};
+
+use std::{thread, time::Duration};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+// Initialize terminal clear screen and return terminal
+pub fn init_term() -> Result<Terminal<CrosstermBackend<io::Stdout>>, io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+
     terminal.clear()?;
-    Ok(())
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(terminal)
 }
